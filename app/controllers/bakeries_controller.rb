950ec323aeaec64919996a6f5564c3f6376896cf
Chris Bendel <chrissbendel@gmail.com>
2025-06-11 10:41:12 -0400

    Rubocop

--- app/controllers/bakeries_controller.rb
+++ app/controllers/bakeries_controller.rb
@@ -1,7 +1,7 @@
 class BakeriesController < ApplicationController
-  before_action :require_user, except: [:index, :show]
-  before_action :set_bakery, only: [:show, :edit, :update]
-  before_action :ensure_owner, only: [:edit, :update]
+  before_action :require_user, except: [ :index, :show ]
+  before_action :set_bakery, only: [ :show, :edit, :update ]
+  before_action :ensure_owner, only: [ :edit, :update ]
 
   def index
     @bakeries = Bakery.all.with_attached_image
